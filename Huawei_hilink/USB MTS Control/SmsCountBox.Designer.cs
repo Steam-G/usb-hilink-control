f2836b5ffd4783f53c0d8366e03efd4075be17ef
L_Andrey <l.andrey@npfgorizont.ru>
2019-09-12 16:47:08 +0500

    Добавил пользовательский элемент для отображения таблицы с данным о сообщениях

---
+++ Huawei_hilink/USB MTS Control/SmsCountBox.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace USB_MTS_Control
+{
+    partial class SmsCountBox
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvSms = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSms)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.dgvSms, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(300, 400);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dgvSms
+            // 
+            this.dgvSms.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSms.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dgvSms.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSms.Location = new System.Drawing.Point(3, 25);
+            this.dgvSms.Name = "dgvSms";
+            this.dgvSms.Size = new System.Drawing.Size(294, 372);
+            this.dgvSms.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "СМС";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Параметр";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Значение";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // SmsCountBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "SmsCountBox";
+            this.Size = new System.Drawing.Size(300, 400);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSms)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dgvSms;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Huawei_hilink/USB MTS Control/SmsCountBox.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace USB_MTS_Control
+{
+    public partial class SmsCountBox : UserControl
+    {
+        private string _LocalUnread;
+        private string _LocalInbox;
+        private string _LocalOutbox;
+        private string _LocalDraft;
+        private string _LocalDeleted;
+        private string _SimUnread;
+        private string _SimInbox;
+        private string _SimOutbox;
+        private string _SimDraft;
+        private string _LocalMax;
+        private string _SimMax;
+        private string _SimUsed;
+        private string _NewMsg;
+
+        public string LocalUnread
+        {
+            get { return _LocalUnread; }
+            set
+            {
+                if (_LocalUnread != value)
+                {
+                    _LocalUnread = value;
+                    string[] record = { "Непрочитанные сообщения", value };
+                    dgvSms.Rows.Add(record);
+                }
+            }
+        }
+
+        public string LocalInbox
+        {
+            get { return _LocalInbox; }
+            set
+            {
+                if (_LocalInbox != value)
+                {
+                    _LocalInbox = value;
+                    string[] record = { "Всего принято", value };
+                    dgvSms.Rows.Add(record);
+                }
+            }
+        }
+
+        public string LocalOutbox
+        {
+            get { return _LocalOutbox; }
+            set
+            {
+                if (_LocalOutbox != value)
+                {
+                    _LocalOutbox = value;
+                    string[] record = { "Отправлено", value };
+                    dgvSms.Rows.Add(record);
+                }
+            }
+        }
+
+        public string LocalDraft
+        {
+            get { return _LocalDraft; }
+            set
+            {
+                if (_LocalDraft != value)
+                {
+                    _LocalDraft = value;
+                    string[] record = { "Черновиков", value };
+                    dgvSms.Rows.Add(record);
+                }
+            }
+        }
+
+        public string LocalDeleted
+        {
+            get { return _LocalDeleted; }
+            set
+            {
+                if (_LocalDeleted != value)
+                {
+                    _LocalDeleted = value;
+                    string[] record = { "Удалено", value };
+                    dgvSms.Rows.Add(record);
+                }
+            }
+        }
+
+        public string SimUnread
+        {
+            get { return _SimUnread; }
+            set
+            {
+                if (_SimUnread != value)
+                {
+                    _SimUnread = value;
+                    string[] record = { "Непрочитанные на Sim", value };
+                    dgvSms.Rows.Add(record);
+                }
+            }
+        }
+
+        public string SimInbox
+        {
+            get { return _SimInbox; }
+            set
+            {
+                if (_SimInbox != value)
+                {
+                    _SimInbox = value;
+                    string[] record = { "Принятых на Sim", value };
+                    dgvSms.Rows.Add(record);
+                }
+            }
+        }
+
+        public string SimOutbox
+        {
+            get { return _SimOutbox; }
+            set
+            {
+                if (_SimOutbox != value)
+                {
+                    _SimOutbox = value;
+                    string[] record = { "Отправленных из Sim", value };
+                    dgvSms.Rows.Add(record);
+                }
+            }
+        }
+
+        public string SimDraft
+        {
+            get { return _SimDraft; }
+            set
+            {
+                if (_SimDraft != value)
+                {
+                    _SimDraft = value;
+                    string[] record = { "Черновики на Sim", value };
+                    dgvSms.Rows.Add(record);
+                }
+            }
+        }
+
+        public string LocalMax
+        {
+            get { return _LocalMax; }
+            set
+            {
+                if (_LocalMax != value)
+                {
+                    _LocalMax = value;
+                    string[] record = { "Максимальный объем", value };
+                    dgvSms.Rows.Add(record);
+                }
+            }
+        }
+
+        public string SimMax
+        {
+            get { return _SimMax; }
+            set
+            {
+                if (_SimMax != value)
+                {
+                    _SimMax = value;
+                    string[] record = { "Максимум на Sim", value };
+                    dgvSms.Rows.Add(record);
+                }
+            }
+        }
+
+        public string SimUsed
+        {
+            get { return _SimUsed; }
+            set
+            {
+                if (_SimUsed != value)
+                {
+                    _SimUsed = value;
+                    string[] record = { "Использовано на Sim", value };
+                    dgvSms.Rows.Add(record);
+                }
+            }
+        }
+
+        public string NewMsg
+        {
+            get { return _NewMsg; }
+            set
+            {
+                if (_NewMsg != value)
+                {
+                    _NewMsg = value;
+                    string[] record = { "Новые сообщения", value };
+                    dgvSms.Rows.Add(record);
+                }
+            }
+        }
+
+        public SmsCountBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
